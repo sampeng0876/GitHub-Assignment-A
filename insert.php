c2de34a57d6b91abbe3651bddbcf8745d32d8a11
Sam Peng <bob0876@outlook.com>
2017-06-14 23:14:46 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,5 @@
 
 
-
 <?php
  $con=mysqli_connect("localhost","bus305_user1","diaoni","bus305_CIS3050");
  // Check connection
@@ -9,9 +8,9 @@
    echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
 
- $sql="INSERT INTO Persons (FirstName, LastName, Age)
+ $sql="INSERT INTO Skills (onetsoc_code, element_id, scale_id, data_value, n, standard_error, lower_ci_bound, upper_ci_bound, recommend_suppress, not_relevant, date_updated, domain_source)
  VALUES
- ('$_POST[firstname]','$_POST[lastname]','$_POST[age]')";
+ ('11-1011.00', '2.A.1.a', 'IM', 4.12, 8, 0.13, 3.88, 4.37, 'N', NULL, '2014-07-01', 'Analyst')";
 
  if (!mysqli_query($con,$sql))
    {
